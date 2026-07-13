ff7457b89a71870b76a3bf9e927da4727b48e042
cesarandev <cucesar6@gmail.com>
2026-07-13 16:24:17 -0500

    Hide marketplace filters on smaller screens

--- update-server/config.example.php
+++ update-server/config.example.php
@@ -7,14 +7,14 @@
 
 return array(
 	'plugin_slug'      => 'cadv-woo-functionalities',
-	'version'          => '1.1.20',
-	'package_file'     => __DIR__ . '/packages/cadv-woo-functionalities-1.1.20.zip',
+	'version'          => '1.1.21',
+	'package_file'     => __DIR__ . '/packages/cadv-woo-functionalities-1.1.21.zip',
 	'homepage'         => 'https://cesarandev.com/',
 	'requires'         => '6.0',
 	'tested'           => '6.6',
 	'requires_php'     => '7.4',
 	'description'      => 'Actualizacion privada de CADV Woo Functionalities.',
-	'changelog'        => '<ul><li>Mejoras responsive para marketplace, CTAs y modal de ficha tecnica.</li></ul>',
+	'changelog'        => '<ul><li>Oculta filtros del marketplace en tablet y movil.</li></ul>',
 	'download_token'   => 'change-this-token',
 	'force_https_urls' => true,
 );
