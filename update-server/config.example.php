e8c58c176dc9fcdb19c163a0aaade1539ab3a0a5
cesarandev <cucesar6@gmail.com>
2026-07-13 15:36:13 -0500

    Use Exo for marketplace filter header

--- update-server/config.example.php
+++ update-server/config.example.php
@@ -7,14 +7,14 @@
 
 return array(
 	'plugin_slug'      => 'cadv-woo-functionalities',
-	'version'          => '1.1.11',
-	'package_file'     => __DIR__ . '/packages/cadv-woo-functionalities-1.1.11.zip',
+	'version'          => '1.1.12',
+	'package_file'     => __DIR__ . '/packages/cadv-woo-functionalities-1.1.12.zip',
 	'homepage'         => 'https://cesarandev.com/',
 	'requires'         => '6.0',
 	'tested'           => '6.6',
 	'requires_php'     => '7.4',
 	'description'      => 'Actualizacion privada de CADV Woo Functionalities.',
-	'changelog'        => '<ul><li>Tipografia Poppins aplicada al marketplace.</li><li>Actualizacion de cache de estilos.</li></ul>',
+	'changelog'        => '<ul><li>Tipografia Exo aplicada al encabezado de filtros.</li><li>Poppins se mantiene en tarjetas y contenido.</li></ul>',
 	'download_token'   => 'change-this-token',
 	'force_https_urls' => true,
 );
