a9a6db1e158f63273da6bb8bd6a30c47ab60e0ad
cesarandev <cucesar6@gmail.com>
2026-07-13 15:42:15 -0500

    Use Poppins for marketplace search filters

--- update-server/config.example.php
+++ update-server/config.example.php
@@ -7,14 +7,14 @@
 
 return array(
 	'plugin_slug'      => 'cadv-woo-functionalities',
-	'version'          => '1.1.13',
-	'package_file'     => __DIR__ . '/packages/cadv-woo-functionalities-1.1.13.zip',
+	'version'          => '1.1.14',
+	'package_file'     => __DIR__ . '/packages/cadv-woo-functionalities-1.1.14.zip',
 	'homepage'         => 'https://cesarandev.com/',
 	'requires'         => '6.0',
 	'tested'           => '6.6',
 	'requires_php'     => '7.4',
 	'description'      => 'Actualizacion privada de CADV Woo Functionalities.',
-	'changelog'        => '<ul><li>Poppins aplicado explicitamente a la lista de categorias del filtro.</li></ul>',
+	'changelog'        => '<ul><li>Poppins aplicado explicitamente al filtro ICA y buscador.</li></ul>',
 	'download_token'   => 'change-this-token',
 	'force_https_urls' => true,
 );
